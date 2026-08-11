58d49aa509872ab507b5aa5e69729aa9c9ac5a96
ChanduCh0604 <chchandrasekharreddy805@gmail.com>
2026-08-11 11:36:03 +0530

    git testing

---
+++ corejavaproject/src/com/corejavaproject/GitTesting.java
@@ -0,0 +1,10 @@
+package com.corejavaproject;
+
+public class GitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi git");
+	}
+
+}
